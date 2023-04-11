ed3c7a4792a6e2a0056e49d43fb0f13dd5ec2c0d
alex2mey <twomj789@newschool.edu>
2023-04-11 17:38:41 -0400

    11/4/2023

--- sessions/week-7/color.js
+++ sessions/week-7/color.js
@@ -1,14 +1,48 @@
 
+const myList = document.querySelector("ul");
+const myRequest = new Request("color.json");
 
+fetch(myRequest)
+  .then((response) => response.json())
+  .then((data) => {
+	console.log(data);
+	console.log(data.colors[1].hexcode);
 
-fetch("color.json")
+    for (const color of data.colors) {
+      const listItem = document.createElement("li");
+      listItem.appendChild(document.createElement("strong")).textContent =
+        color.colorname;
+      listItem.append(` has the following hex value ${color.hexcode} `);
+      listItem.appendChild(
+        document.createElement("strong")
+      ).style.background = `${color.hexcode}`;
+      myList.appendChild(listItem);
+    }
+	console.log(data.colors[1].hexcode);
+
+  })
+  .catch(console.error);
+
+/* fetch("color.json")
 .then(function(response){
 	return response.json();
 })
 .then(function(products){
-	let placeholder = document.querySelector("#data-output");
-	let out = "";
-	for(let product of products){
+	data.forEach( function(item,index){
+		let placeholder = document.querySelector("#data-output");
+		let out = "";
+		let newItem = document.createElement("div");
+		newItem.classList.add(item.color);}
+
+		newItem.innerHTML = `
+		<div class="grid">
+				<div>color name ${item.colorname}</div>
+				<div>hexcode ${item.hexcode}</div>
+				<div class ="backgroundcolor">color ${item.color}</div>
+			</div>`;
+
+
+ for(let product of products){
 		out += `
 			<tr>
 				<td>${product.colorname}</td>
@@ -19,4 +53,8 @@ fetch("color.json")
 	}
 
 	placeholder.innerHTML = out;
-});
+
+	/*newItem.innerHTML=`
+	<span class="color">${item.color}${item.hexvalue}</span>`;
+	container.appendChild
+}); */
